c84b0343cc03de75ed417c17443273f3088c0c25
Temitope Akande <topsiology11@gmail.com>
2023-05-15 15:48:46 +0100

    debug

--- app/page.js
+++ app/page.js
@@ -46,9 +46,9 @@ export default function Home() {
 
     // If user is not connected to the Goerli network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 11155111) {
-      window.alert("Change the network to Sepolia");
-      throw new Error("Change network to Sepolia");
+    if (chainId !== 5) {
+      window.alert("Change the network to Goerli");
+      throw new Error("Change network to Goerli");
     }
     const signer = web3Provider.getSigner();
     // Get the address associated to the signer which is connected to  MetaMask
@@ -96,7 +96,7 @@ export default function Home() {
       // Assign the Web3Modal class to the reference object by setting it's `current` value
       // The `current` value is persisted throughout as long as this page is open
       web3ModalRef.current = new Web3Modal({
-        network: "sepolia",
+        network: "goerli",
         providerOptions: {},
         disableInjectedProvider: false,
       });
